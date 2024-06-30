c271f5dc229afd60db36c0278b682a43c55f3da7
unknown <vaibh18@gmail.com>
2024-06-30 10:52:25 +0530

    chore: added yup validations on the signup screen

---
+++ components/useSignupFormValidation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as Yup from 'yup';
+
+const useSignupFormValidation = () => {
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Password is required'),
+  });
+
+  return { validationSchema };
+};
+
+export default useSignupFormValidation;
