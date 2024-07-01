884a20ee2df7c6b3388331d37000a45a5116b9ac
Arun Renniy Dsa <arun.r.dsa@gmail.com>
2024-07-01 16:55:43 +0530

    added metrics to home screen

--- screens/ExpenseListing.js
+++ screens/ExpenseListing.js
@@ -34,7 +34,7 @@ const ExpenseListing = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: '#307ecc',
     alignItems: 'center',
     justifyContent: 'center',
     paddingRight: 10,
